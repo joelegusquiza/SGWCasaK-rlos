a5966f631ba9ea7987c1f716778aa6236c2a7372
Joel Egusquiza <joelegusquizaacosta@gmail.com>
2018-12-05 22:32:12 -0300

    init project

---
+++ SGWCasaK-rlos/SGWCasaK-rlos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SGWCasaK_rlos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
